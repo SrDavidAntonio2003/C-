519311c9196e35511e56abf5d48e78e4d38952c8
David <happywybie@gmail.com>
2022-06-08 14:59:55 -0600

    V1

---
+++ + o -.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Eje1sel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("De un numero: ");
+            num = int.Parse(Console.ReadLine());
+            if (num > 0)
+                Console.WriteLine("Es negativo...");
+            else
+                if (num < 0)
+                Console.WriteLine("Es negativo...");
+            else
+                Console.WriteLine("Es cero...");
+
+            Console.ReadKey();
+        }
+    }
+}
